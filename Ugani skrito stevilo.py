eac69e226bbb3207481a63b658fb28de16090be9
OblakM <44818866+OblakM@users.noreply.github.com>
2018-12-11 22:29:53 +0100

    Update Ugani skrito stevilo.py

--- Ugani skrito stevilo.py
+++ Ugani skrito stevilo.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 secret = 7
 
-guess = int(raw_input("Ugani skrito stevilo: "))
+guess = int(raw_input("Ugani skrito število: "))
 
 if guess == secret:
-    print "Bravo, uganil si skrito stevilo. To stevilo je " + str(secret) + "."
+    print "Bravo, uganil si skrito število. To število je " + str(secret) + "."
 
 if guess != secret:
-    print "Napacen odgovor, poskusi ponovno."
+    print "Napačen odgovor, več sreče prihodnjič."
